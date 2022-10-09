9c12ce2a9d4fbfb9fa9ad331e1607544474b2413
Chong Tan <31427334+PenguinBoyTC@users.noreply.github.com>
2022-10-08 23:12:47 -0700

    Create  PDF and CCDF

--- Project.py
+++ Project.py
@@ -2,10 +2,11 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import pandas as pd
 import json
+import numpy as np
 
 FEATURES = ['ID', 'Severity', 'Zipcode', 'Sunrise_Sunset', 'Temperature(F)', 'Humidity(%)', 'Pressure(in)',
             'Visibility(mi)', 'Wind_Speed(mph)', 'Precipitation(in)', 'Weather_Condition']
-THRESHOLD = 8
+THRESHOLD = 7
 FILE_NAME = 'US_Accidents_Dec21_updated.csv'
 VALID_DATA = 'f10000_valid_data.csv'
 EDGE_DICT = f'edge_dict_{THRESHOLD}.json'
@@ -23,16 +24,21 @@ def read_data(filename):
     return df
 
 
-def create_graph(edges_df):
+def draw_graph(G):
     options = {
         'node_size': 1,
         'with_labels': False
     }
+    nx.draw(G, **options)
+    plt.show()
+
+
+def create_graph(edges_df):
     G = nx.from_pandas_edgelist(edges_df, edge_attr=None)
     undirect_G = G.to_undirected()
-    nx.write_gml(undirect_G, f'threshold_{THRESHOLD}.gml')
-    # nx.draw(undirect_G, **options)
-    # plt.show()
+    draw_graph(undirect_G)
+    # nx.write_gml(undirect_G, f'threshold_{THRESHOLD}.gml')
+    return undirect_G
 
 
 def convert_df_to_edge_dict(df, output_file):
@@ -71,17 +77,84 @@ def convert_df_to_edge_dict(df, output_file):
         json.dump(edge_dict, outfile)
 
 
+def read_graph_from_GML(filename):
+    G = nx.read_gml(filename)
+    undirect_G = G.to_undirected()
+    return undirect_G
+
+
 # acc_df = read_data(VALID_DATA)
 # convert_df_to_edge_dict(acc_df, EDGE_DICT)
-with open(EDGE_DICT) as json_file:
-    edge_dict = json.load(json_file)
-    edges_df = pd.DataFrame(edge_dict)
-    create_graph(edges_df)
+# with open(EDGE_DICT) as json_file:
+#     edge_dict = json.load(json_file)
+#     edges_df = pd.DataFrame(edge_dict)
+#     create_graph(edges_df)
 
 
-# edges_df = pd.DataFrame(
-#     edge_dict
-# )
+undirect_G = read_graph_from_GML(f'threshold_{THRESHOLD}.gml')
 
-# create_graph(edges_df)
-# acc_df = read_data('f10000_valid_data.csv')
+sum = 0
+for node, degree in nx.degree(undirect_G):
+    sum += degree
+average_degree = sum / undirect_G.number_of_nodes()
+# average_path_length = nx.average_shortest_path_length(undirect_G)
+# diameter = nx.diameter(undirect_G)
+average_clustering = nx.average_clustering(undirect_G)
+global_clustering = nx.transitivity(undirect_G)
+print("average degree: ", average_degree)
+# print("average path length: ", average_path_length)
+# print("diameter: ", diameter)
+print("average clustering: ", average_clustering)
+print("global clustering: ", global_clustering)
+
+
+def CCDF(nums):
+    n = np.array(nums)
+    cdf = n.cumsum(0)
+    l = cdf[-1]
+    cdf = cdf * 1.0 / l
+    ccdf = 1 - cdf
+    return ccdf
+
+
+def plot_CDDF(g):
+    degree_dict = nx.degree_histogram(g)
+    np_array = np.array(degree_dict)
+    ents = []
+    nums = []
+    for degree, count in enumerate(np_array):
+        ents.append(degree)
+        nums.append(count)
+    ccdf = CCDF(nums)
+    plt.plot(ents, ccdf)
+    plt.xlabel('Degree')
+    plt.ylabel('CCDF')
+    plt.xscale("log")
+    plt.yscale("log")
+    plt.title('CCDF of Degree Distribution')
+    plt.show()
+
+
+def plot_PDF(g, normalized=True):
+    print("Creating histogram...")
+    aux_y = nx.degree_histogram(g)
+
+    aux_x = np.arange(0, len(aux_y)).tolist()
+
+    n_nodes = g.number_of_nodes()
+
+    if normalized:
+        for i in range(len(aux_y)):
+            aux_y[i] = aux_y[i]/n_nodes
+
+        plt.title('\nDistribution Of Node Linkages (log-log scale)')
+        plt.xlabel('Degree\n(log scale)')
+        plt.ylabel('Number of Nodes\n(log scale)')
+        plt.xscale("log")
+        plt.yscale("log")
+        plt.plot(aux_x, aux_y, 'o')
+        plt.show()
+
+
+plot_PDF(undirect_G)
+plot_CDDF(undirect_G)
